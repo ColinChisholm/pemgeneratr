ca7cf688a4018c6794d7b5e97fd84270136b80fa
Colin Chisholm <colin.chisholm@unbc.ca>
2020-03-02 08:03:49 -0800

    restoring model_gen.R

---
+++ R/model_gen.R
@@ -0,0 +1,49 @@
+#' Generate a machine learning model
+#'
+#' This function takes in all the data needed to produce machine learning model.
+#' Inputs are handed to a RMD report/ script.
+#' Outputs include the markdown report, the cross validation object,
+#' and a binary model (RDS) that can then be used to predict on new data.
+#'
+#'
+#' @param outDir  Highly recommended to be set as an absolute directory.  This defaults to the project's root directory OR where the RMD script is saved.
+#' Additional products generated from the associated<tt>model_gen.Rmd</tt> markdown script will also be saved to this dir.
+#' @param traindat Is a dataframe that contains the model training data.  The reponse variable should be one of the columns.
+#' @param target   The name of the response variable in the traindat data frame.
+#' @param rseed    Optional random number seed.
+#' @keywords machine-learning, model, report
+#' @export
+#' @examples
+#' dat <- read.csv("e:/workspace/2020/PEM/ALRF_PEMv2/dev/modDat.csv",
+#'                stringsAsFactors = TRUE)
+#'
+#'
+#' model_gen(traindat = dat,
+#'           target = "SiteSeries",
+#'           outDir = "e:/tmp/model_gen_test",
+#'           rseed = 456)
+
+
+model_gen <- function(traindat, target, outDir = ".", rseed = NA) {
+  ## create destination folder
+  ifelse(!dir.exists(file.path(outDir)),                # if folder does not exist
+          dir.create(file.path(outDir)), FALSE)         # create it
+
+  ## call report -- passing variables to it
+  RMD <- system.file("rmd", "model_gen.Rmd", package = "pemgeneratr") ## this syntax designed for a package install.
+  # RMD <- "./R/model_gen.Rmd"  ## manually set for sourcing this function
+
+  rmarkdown::render(RMD,              ## where the rmd is located
+                    params = list(traindat = traindat,  ## parameters to send to rmarkdown
+                                  target   = target,
+                                  outDir = outDir,
+                                  rseed = rseed),
+                    output_dir = outDir)                ## where to save the report
+
+  ## open the report
+  browseURL(paste(outDir, "model_gen.html", sep = "/"))
+}
+
+
+
+
