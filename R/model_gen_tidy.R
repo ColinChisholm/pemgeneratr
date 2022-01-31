27de9d65c0d2cb1f2af55b50fe2bf84e7a4595e8
Colin E Chisholm <colin.chisholm@unbc.ca>
2022-01-31 15:20:55 -0800

    upgrading to Gen's newest version for model generation

--- R/model_gen_tidy.R
+++ R/model_gen_tidy.R
@@ -6,65 +6,67 @@
 #' and a binary model (RDS) that can then be used to predict on new data.
 #'
 #'
+#' @param trDat   Is a dataframe that contains the model training data.  The response variable should be one of the columns.
 #' @param outDir  Highly recommended to be set as an absolute directory.  This defaults to the project's root directory OR where the RMD script is saved.
 #' Additional products generated from the associated `model_gen_tidy.Rmd`` markdown script will also be saved to this dir.
-#' @param trDat Is a dataframe that contains the model training data.  The reponse variable should be one of the columns.
-#' @param target   The name of the response variable in the traindat data frame.
+#' @param mname    Name for this model run.  Will be used to name outputs.
+#' @param target   The name of the response variable in the trDat data frame.
+#' @param target2  A second target
+#' @param tid      The transect id. Used for ...
+#' @param field_transect WHAT IS THIS?
+#' @param infiles  Simply for reporting -- to specify what files were used in the creation of trDat.
+#' @param mmu      Map unit (e.g. BC BEC subzone).  This may be a column in the input data and will allow for the processing of multiple subzones in one model run.
+#' @param ds_ratio Covariate/predictor variable balancing: downsample proportion
+#' @param sm_ratio Covariate/predictor variable balancing: Smote proportion
 #' @param rseed    Optional random number seed.
-#' @keywords machine-learning, model, report
+#' @keywords       machine-learning, random forest, model, report
 #' @export
 #' @examples
 #'
 #'
 
-model_gen_tidy <- function(trDat, 
-                           target, 
-                           target2, 
-                           tid, 
-                           outDir = ".", 
-                           mname = "model", 
-                           rseed = NA, 
-                           infiles = infiles, 
+model_gen_tidy <- function(trDat,
+                           outDir = ".",
+                           mname = "Model",
+                           target,
+                           target2,
+                           tid,
+                           ds_ratio,
+                           sm_ratio,
+                           rseed = NA,
+                           infiles = infiles,
                            mmu = mmu,
                            field_transect = field_transect){
-# # # testing : GP
-# trDat = inmdata_all
-#  target = "target"
-#  target2 = "target2"
-#  tid = "tid"
-#  outDir =  outDir
-#  infiles = infiles
-#  rseed = 456
-#  mmu = mmu
-#  mname = mname
-
-   ## create destination folder
+  ## create destination folder
   ifelse(!dir.exists(file.path(outDir)),                # if folder does not exist
           dir.create(file.path(outDir)), FALSE)         # create it
 
   ## error testing ----------------
- # if (sum(is.na(trDat[,target])) > 0) {
-#    stop(paste("There are,", sum(is.na(trDat[,target]))  , "NA values in the target:", target))
-#  }
+  if (sum(is.na(trDat[,target])) > 0) {
+     stop(paste("There are,", sum(is.na(trDat[,target]))  , "NA values in the target:", target))
+   }
 
 
-  RMD <- "D:/PEM_DATA/BEC_DevExchange_Work/_functions/model_gen_tidy.Rmd"
+  #RMD <- "D:/PEM_DATA/BEC_DevExchange_Work/_functions/model_gen_tidy.Rmd"
   #RMD <- "D:/GitHub/PEM_Methods_DevX/_functions/model_gen_tidy.Rmd"
 
-  rmarkdown::render(RMD,              ## where the rmd is located
-                    params = list(trDat = trDat,  ## parameters to send to rmarkdown
-                                  target = target,
-                                  target2 = target2,
-                                  tid = tid, 
-                                  outDir = outDir,
-                                  infiles = infiles,
-                                  mmu = mmu,
-                                  rseed = rseed,
-                                  mname = mname,
-                                  field_transect = field_transect),
-                    output_dir = outDir)                ## where to save the report
-  
-  file.rename(paste0(outDir,"/", "model_gen_tidy.html"), paste0(outDir,"/", mname,"_treport.html"))
+  rmarkdown::render("model_gen_tidy_2201.Rmd",              ## where the rmd is located
+                    params = list(
+                      trDat     = trDat,
+                      outDir    = outDir,
+                      mname     = mname,
+                      target    = target,
+                      target2   = target2,
+                      tid       = tid,
+                      ds_ratio  = ds_ratio,
+                      sm_ratio  = sm_ratio,
+                      rseed     = rseed,
+                      infiles   = infiles,
+                      mmu       = mmu,
+                      field_transect = field_transect),
+                  output_dir = outDir)                ## where to save the report
+
+  file.rename(paste0(outDir,"/", "model_gen_tidy.html"), paste0(outDir,"/", mname,"_report.html"))
   ## open the report
-  browseURL(paste0(paste0(outDir,"/", mname,"_treport.html")))
+  browseURL(paste0(paste0(outDir,"/", mname,"_report.html")))
 }
